6d834d3c8681c411cb067029eca24e02e5df6e89
DrMatters <qseminq@gmail.com>
2017-10-18 20:08:00 +0300

    Added some tests

--- src/test/com/td/api/ApiTest.java
+++ src/test/com/td/api/ApiTest.java
@@ -11,9 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,6 +41,8 @@ public class ApiTest {
             0L
     );
 
+
+
     private TestUserDto temporaryUser = new TestUserDto(
             "1234567",
             "1234567",
@@ -58,34 +58,101 @@ public class ApiTest {
 
     @Test
     public void signupSuccessful() {
-        HttpEntity<TestUserDto> requestEntity = new HttpEntity<>(temporaryUser);
-        ResponseEntity<UserDto> registerResponse =
-                restTemplate.postForEntity("/auth/signup", requestEntity, UserDto.class);
-
-        assertEquals(HttpStatus.CREATED, registerResponse.getStatusCode());
-
-        List<String> cookies = registerResponse.getHeaders().get("Set-Cookie");
-        assertNotNull(cookies);
-        assertFalse(cookies.isEmpty());
-
-        UserDto user = registerResponse.getBody();
-        assertNotNull(user);
-
-        assertNotNull(user.getId());
-        temporaryUser.setId(user.getId());
-
-        assertEquals(temporaryUser.getEmail(), user.getEmail());
-        assertEquals(temporaryUser.getLogin(), user.getLogin());
+        hardRegister(temporaryUser);
     }
 
-    //@Test
+    @Test
     public void signinSuccesful() {
         login(registeredUser);
     }
 
-    private List<String> login(TestUserDto generatedUser) {
-        hardRegister(generatedUser);
-        HttpEntity<TestUserDto> requestEntity = new HttpEntity<>(generatedUser);
+    @Test
+    public void logoutSuccessful() {
+        List<String> cookies = login(registeredUser);
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.put(HttpHeaders.COOKIE, cookies);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(requestHeaders);
+
+        ResponseEntity<UserDto> logoutResponse =
+                restTemplate.exchange("/auth/logout", HttpMethod.GET, requestEntity, UserDto.class);
+
+        assertEquals(HttpStatus.OK, logoutResponse.getStatusCode());
+
+        UserDto user = logoutResponse.getBody();
+        assertNotNull(user);
+
+        assertEquals(registeredUser.getEmail(), user.getEmail());
+        assertEquals(registeredUser.getLogin(), user.getLogin());
+        assertEquals(registeredUser.getId(), user.getId());
+
+    }
+
+    @Test
+    public void getUserInfo() {
+        List<String> cookies = login(registeredUser);
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.put(HttpHeaders.COOKIE, cookies);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(requestHeaders);
+
+        ResponseEntity<UserDto> infoResponse =
+                restTemplate.exchange("/user", HttpMethod.GET, requestEntity, UserDto.class);
+
+        assertEquals(HttpStatus.OK, infoResponse.getStatusCode());
+
+        UserDto user = infoResponse.getBody();
+        assertNotNull(user);
+
+        assertEquals(registeredUser.getEmail(), user.getEmail());
+        assertEquals(registeredUser.getLogin(), user.getLogin());
+        assertEquals(registeredUser.getId(), user.getId());
+    }
+
+    @Test
+    public void editUserInfo() {
+        TestUserDto userToEdit = new TestUserDto(
+                "abcdefgh",
+                "abcdefgh",
+                "abcdefgh@mail.ru",
+                0L
+        );
+        //register user with this fields
+        List<String> cookies = hardRegister(userToEdit);
+
+        TestUserDto editedUser = userToEdit;
+
+        //change fields which is available to edit
+        userToEdit.setLogin("abcdefgh1");
+        userToEdit.setPassword("abcdefgh1");
+        userToEdit.setEmail("abcdefgh1@mail.ru");
+
+        //creating request body and headers, also putting cookies into request headers
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.put(HttpHeaders.COOKIE, cookies);
+        HttpEntity<TestUserDto> requestEntity = new HttpEntity<>(editedUser, requestHeaders);
+
+        //receiving response from controller
+        ResponseEntity<UserDto> editResponse =
+                restTemplate.postForEntity("/user/edit", requestEntity, UserDto.class);
+
+        assertEquals(HttpStatus.OK, editResponse.getStatusCode());
+
+        //checking cookies, and resetting it
+        cookies = editResponse.getHeaders().get("Set-Cookie");
+        assertNotNull(cookies);
+        assertFalse(cookies.isEmpty());
+
+        UserDto user = editResponse.getBody();
+        assertNotNull(user);
+        
+        assertEquals(editedUser.getEmail(), user.getEmail());
+        assertEquals(editedUser.getLogin(), user.getLogin());
+        assertEquals(editedUser.getId(), user.getId());
+    }
+
+    private List<String> login(TestUserDto userToLogin) {
+        HttpEntity<TestUserDto> requestEntity = new HttpEntity<>(userToLogin);
         ResponseEntity<UserDto> loginResponse =
                 restTemplate.postForEntity("/auth/signin", requestEntity, UserDto.class);
 
@@ -98,13 +165,13 @@ public class ApiTest {
         UserDto user = loginResponse.getBody();
         assertNotNull(user);
 
-        assertEquals(generatedUser.getEmail(), user.getEmail());
-        assertEquals(generatedUser.getLogin(), user.getLogin());
-        assertEquals(generatedUser.getId(), user.getId());
+        assertEquals(userToLogin.getEmail(), user.getEmail());
+        assertEquals(userToLogin.getLogin(), user.getLogin());
+        assertEquals(userToLogin.getId(), user.getId());
         return cookies;
     }
 
-    private void hardRegister(TestUserDto userToRegister) {
+    private List<String> hardRegister(TestUserDto userToRegister) {
         HttpEntity<TestUserDto> requestEntity = new HttpEntity<>(userToRegister);
         ResponseEntity<UserDto> registerResponse =
                 restTemplate.postForEntity("/auth/signup", requestEntity, UserDto.class);
@@ -115,7 +182,6 @@ public class ApiTest {
         assertNotNull(cookies);
         assertFalse(cookies.isEmpty());
 
-
         UserDto user = registerResponse.getBody();
         assertNotNull(user);
 
@@ -124,5 +190,7 @@ public class ApiTest {
 
         assertEquals(userToRegister.getEmail(), user.getEmail());
         assertEquals(userToRegister.getLogin(), user.getLogin());
+
+        return cookies;
     }
 }
